5536bc9ecd4d065ee0d3555a7aad97ea5e60d767
arturomp99 <arturo.m.p.99@gmail.com>
2024-04-15 01:58:54 +0200

    feat: CATALOG query by species

--- src/routes/catalog.ts
+++ src/routes/catalog.ts
@@ -7,6 +7,11 @@ catalogRouter.get("/data", (_req, res) => {
   res.json(catalogController.getCatalogData());
 });
 
+catalogRouter.get("/species/:scientificName", (req, res) => {
+  const scientificName = req.params.scientificName;
+  res.json(catalogController.getCatalogEntry(scientificName));
+});
+
 catalogRouter.get("/total", (_req, res) =>
   res.json(catalogController.getTotalCatalogInfo())
 );
